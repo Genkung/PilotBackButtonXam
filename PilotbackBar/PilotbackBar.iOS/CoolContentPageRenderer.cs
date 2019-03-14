ec75cf4d661117257934871b312d4eb2dcec5643
parada <parada@perfenterprise.com>
2019-03-14 10:12:09 +0700

    Add project.

---
+++ PilotbackBar/PilotbackBar.iOS/CoolContentPageRenderer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CoreGraphics;
+using PilotbackBar;
+using PilotbackBar.iOS;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(CoolContentPage), typeof(CoolContentPageRenderer))]
+namespace PilotbackBar.iOS
+{
+    public class CoolContentPageRenderer : PageRenderer
+    {
+        public override void ViewWillAppear(bool animated)
+        {
+            base.ViewWillAppear(animated);
+
+           // SetCustomBackButton();
+        }
+
+        private void SetCustomBackButton()
+        {
+            var backBtnImage = UIImage.FromBundle("iosbackarrow.png");
+
+            backBtnImage = backBtnImage.ImageWithRenderingMode(UIImageRenderingMode.AlwaysTemplate);
+
+            var backButtonItemWithImage = new UIBarButtonItem(backBtnImage, UIBarButtonItemStyle.Plain, BtnClick)
+            {
+                ImageInsets = new UIEdgeInsets(0f, -8f, 0f, 0f)
+            };
+
+            UIBarButtonItem[] bArray = { backButtonItemWithImage };
+
+            NavigationController.TopViewController.NavigationItem.SetLeftBarButtonItems(bArray,false);
+        }
+
+        private void BtnClick(object sender, EventArgs e)
+        {
+            if (((CoolContentPage)Element)?.CustomBackButtonAction != null)
+            {
+                ((CoolContentPage)Element)?.CustomBackButtonAction.Invoke();
+            }
+            else App.Current.MainPage.Navigation.PopAsync();
+        }
+    }
+}
---
+++ PilotbackBar/PilotbackBar/Page3.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PilotbackBar
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Page3 : CoolContentPage
+    {
+		public Page3 ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
